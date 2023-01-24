3606651e2b99de297e9b94601a731e1144cede50
kuya <k.oda.sys22@morijyobi.ac.jp>
2023-01-24 13:50:02 +0900

    削除機能の作成

---
+++ src/main/java/dto/Book.java
@@ -0,0 +1,70 @@
+package dto;
+
+public class Book {
+	private int id;
+	private String name;
+	private String writer;
+	private String pub;
+	private int ISBN;
+	private int Login_id;
+	
+	public Book(int id,String name,String writer,String pub,int ISBN,int Login_id) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.writer = writer;
+		this.pub = pub;
+		this.ISBN = ISBN;
+		this.Login_id = Login_id;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getPub() {
+		return pub;
+	}
+
+	public void setPub(String pub) {
+		this.pub = pub;
+	}
+
+	public int getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(int iSBN) {
+		ISBN = iSBN;
+	}
+
+	public int getLogin_id() {
+		return Login_id;
+	}
+
+	public void setLogin_id(int login_id) {
+		Login_id = login_id;
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/view/delete-form.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="DeleteActionServlet" method="post">
+	<p>削除する図書のIDを入力してください</p>
+	<input type="text" name="delid">
+	<input type="submit">
+	</form>
+</body>
+</html>
